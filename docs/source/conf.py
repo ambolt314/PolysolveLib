379923788ad5af0c8c71118ad0095d33eaabf7ac
Andrew Bolt <40577677+ambolt314@users.noreply.github.com>
2025-09-05 16:58:01 +0100

    Add test folders; documentation;

---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import polysolve
+from datetime import date
+
+project = 'polysolve'
+copyright = f'{polysolve.__author__}, {date.today().year}'
+author = 'Andrew Bolt'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx.ext.autosummary", "sphinx_autodoc_typehints", "sphinx.ext.intersphinx"]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'numpy': ('https://docs.scipy.org/doc/numpy', None)
+}
---
+++ docs/source/usage.rst
@@ -0,0 +1,26 @@
+Usage
+=====
+
+Polysolve is able to solve quadratic and cubic equations for any complex numbers `a`, `b`, `c`, `d`
+
+Quadratic
+---------
+Solves the roots of a quadratic equation in the form :math:`ax^2 + bx + c`
+
+    :a: coefficient of :math:`x^2` term
+    :b: coefficient of :math:`x` term
+    :c: constant term
+
+returns all possible complex solutions
+
+
+Cubic
+--------
+Solves the roots of a cubic equation in the form :math:`ax^3 + bx^2 + cx + d`
+
+    :a: coefficient of :math:`x^3` term
+    :b: coefficient of :math:`x^2` term
+    :c: coefficient of :math:`x` term
+    :d: constant term
+
+returns all possible complex solutions
